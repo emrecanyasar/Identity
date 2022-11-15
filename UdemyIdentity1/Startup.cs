b2e86c8435bfbf3bcd7acaff2d1fe4ac3019e5bf
Emrecan <emrecanyasar8@outlook.com>
2022-11-15 23:26:38 +0300

    some fixes

--- UdemyIdentity1/Startup.cs
+++ UdemyIdentity1/Startup.cs
@@ -1,24 +1,22 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UdemyIdentity
+namespace UdemyIdentity1
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddMvc();
         }
 
@@ -28,14 +26,7 @@ namespace UdemyIdentity
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.UseCors();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
