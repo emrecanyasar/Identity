31c25b9725d15944ac7286206243f6a0d110529a
Emrecan <emrecanyasar8@outlook.com>
2022-11-16 00:07:08 +0300

    some fixes

--- UdemyIdentity1/Startup.cs
+++ UdemyIdentity1/Startup.cs
@@ -1,21 +1,38 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemyIdentity.Models;
 
 namespace UdemyIdentity1
 {
     public class Startup
     {
+        public IConfiguration configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppIdentityDbContext>(opts =>
+            {
+                opts.UseSqlServer(configuration["ConnectionStrings:DefaultConnectionString"]);
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+
             services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddMvc();
         }
@@ -27,6 +44,7 @@ namespace UdemyIdentity1
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
         }
     }
 }
