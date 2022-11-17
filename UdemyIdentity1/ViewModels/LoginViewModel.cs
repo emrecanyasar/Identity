b2938d0a2c054f39041e748809cbccb5875788a9
Emrecan <emrecanyasar8@outlook.com>
2022-11-18 00:42:30 +0300

    Login page created

---
+++ UdemyIdentity1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyIdentity.ViewModels
+{
+    public class LoginViewModel 
+    {
+        [Required(ErrorMessage = "Email Alanı Gereklidir!")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Email adresiniz doğru değil.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Şifre Gereklidir.")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        [MinLength(4,ErrorMessage ="Şifreniz en az 4 karakterli olmalıdır.")]
+        public string Password { get; set; }
+    }
+}
