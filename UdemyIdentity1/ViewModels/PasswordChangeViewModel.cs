dcafdef79c2baa13acc179e6a2e66bbe46da95a5
Emrecan <emrecanyasar8@outlook.com>
2022-11-20 15:03:56 +0300

    User change password pages created

---
+++ UdemyIdentity1/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyIdentity.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [Display(Name ="Eski Şifreniz")]
+        [Required(ErrorMessage ="Eski şifreniz gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(1,ErrorMessage ="Şifreniz en az 4 karakterli olmalıdır.")]
+        public string PasswordOld { get; set; }
+        [Display(Name = "Yeni Şifreniz")]
+        [Required(ErrorMessage = "Yeni şifreniz gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(1, ErrorMessage = "Şifreniz en az 4 karakterli olmalıdır.")]
+        public string PasswordNew { get; set; }
+        [Display(Name = "Tekrar yeni Şifreniz")]
+        [Required(ErrorMessage = "Yeni şifre tekrar gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(1, ErrorMessage = "Şifreniz en az 4 karakterli olmalıdır.")]
+        [Compare("PasswordNew",ErrorMessage ="Yeni şifreniz onay şifrenizle aynı değildir")]
+        public string PasswordConfirm { get; set; }
+    }
+}
